bfc0b131ea3e70b09188337e68c75966dec8760d
Pablo <p.ramirezt2@uniandes.edu.co>
2025-09-25 22:40:12 -0500

    1

--- monitoring/urls.py
+++ monitoring/urls.py
@@ -3,7 +3,6 @@ from django.urls import path, include
 from inventory.views import health_check
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
     path('', health_check),              
     path('', include('inventory.urls')),  
 ]
