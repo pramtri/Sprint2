fb6035cf86499794ef86dbe70c5562639fa6d9d0
Pablo <p.ramirezt2@uniandes.edu.co>
2025-10-16 21:32:56 -0500

    cambios

--- monitoring/urls.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from django.urls import path, include
-from inventory.views import health_check
-
-urlpatterns = [
-    path('', health_check),              
-    path('', include('inventory.urls')),  
-]
